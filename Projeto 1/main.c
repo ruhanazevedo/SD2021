531f72e7da8bfb81c95b5815fb6f4705e0e39b2e
Ruhan Azevedo <37676775+ruhanvictor@users.noreply.github.com>
2021-09-23 16:14:11 +0100

    Added makeFile, main.c and first imp of data.c

---
+++ Projeto 1/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "headers/data.h"
+//#include "src/data.c"
+
+int main(void){
+    struct data_t * x = data_create(2);
+    //char str[] = "test\n";
+    //printf("%s", str);
+}
---
+++ Projeto 1/src/data.c
@@ -0,0 +1,15 @@
+#include "../headers/data.h"
+
+struct data_t *data_create(int size){
+    data_t *new_data;
+    new_data = malloc(size * sizeof(data_t));
+    return new_data;
+}
+
+struct data_t *data_create2(int size, void *data); 
+
+void data_destroy(struct data_t *data);
+
+struct data_t *data_dup(struct data_t *data);
+
+void data_replace(struct data_t *data, int new_size, void *new_data);
